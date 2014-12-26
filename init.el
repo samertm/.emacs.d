54d4b9bfe0fe9f16b7fce7d5419de0df93de0f9a
Samer Masterson <samer@samertm.com>
2014-12-26 14:11:15 -0800

    add erc modules

--- init.el
+++ init.el
@@ -28,7 +28,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(erc-modules
+   (quote
+    (completion netsplit fill button match track readonly networks ring autojoin noncommands irccontrols move-to-prompt stamp menu list))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
