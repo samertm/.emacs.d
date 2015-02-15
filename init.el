4314549bd66e335706c7a572a087c073253dfb57
Samer Masterson <samer@samertm.com>
2015-02-14 18:50:18 -0800

    update

--- init.el
+++ init.el
@@ -2,9 +2,11 @@
 
 ;; suppress gui & startup
 (setq inhibit-startup-screen t)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
+(if window-system
+    (progn
+      (tool-bar-mode -1)
+      (menu-bar-mode -1)
+      (scroll-bar-mode -1)))
 (column-number-mode t)
 
 ;; for chromebook
